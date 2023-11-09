2f04d1df84ce4d6c795ff6ba954801fdbb7a5c19
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-11-09 09:54:57 +0100

    add prompt to closing file

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,13 +3,29 @@ import { invoke } from '@tauri-apps/api'
 import { onMounted, ref } from 'vue';
 import { CommandData } from '../CommandData';
 import { message } from '@tauri-apps/api/dialog';
+import vueQr from 'vue-qr/src/packages/vue-qr.vue'
+import router from '../router';
 
 const serverAddress = ref("");
+const isConnectedToWifi = ref(false);
+
 onMounted(async ()=>{
 await invoke('server').then((response) =>{
   const ipcResponse = response as unknown as CommandData<string>;
   serverAddress.value = ipcResponse.data as unknown as string
 })
+
+//TODO: fetch data from backend 
+await invoke('is_connected_to_wifi').then((response) =>{
+  const ipcResponse = response as unknown as CommandData<boolean>;
+  isConnectedToWifi.value = ipcResponse.data as unknown as boolean
+})
+
+//TODO: reroute on successfully connection
+function qrScanCallback(){
+  // route to /upload
+  router.push({path:"/upload"})
+}
 
 // show a message dialog
 await message(`visit ${serverAddress.value} on the connected device`, { title: 'skylite', type: 'info' });
@@ -17,15 +33,30 @@ await message(`visit ${serverAddress.value} on the connected device`, { title: '
 </script>
 
 <template>
-  <div class="dark:text-gray-400">
+  <div class="dark:text-gray-400" v-if="!isConnectedToWifi">
     <p class="capitalize left text-purple mt-10 text-[#FF3D00]">Waiting for connection</p>
     <p class="mt-4 py-4 w-3/4 dark:text-gray-400 leading-1">
       You should see the file transfer begin as soon as someone start transferring to your computer from their Wifi
       enabled device
     </p>
- 
+
     <div class="flex flex-col items-center justify-center mt-[100px]">
       <span class="loader"></span>
+    </div>
+
+
+
+
+
+
+  </div>
+  <div v-else class="flex flex-col items-center justify-center h-[100%]">
+    <div class="flex flex-col items-center justify-center mt-[35px]">
+      <div>
+        <vue-qr bgSrc='/satellite.png' dot-scale="1" class="border-r-2 mb-3" :text="serverAddress" :size="200"></vue-qr>
+      </div>
+      <h2 class="dark:text-gray-400">scan for visit <span class="text-[#FF3D00]">{{ serverAddress }}</span>
+      </h2>
     </div>
   </div>
 </template>
