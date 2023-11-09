0faa42549d629fb53e850bb3d54ce8420fb52be7
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-11-09 08:30:32 +0100

    update

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use ipc;
 use server;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -14,7 +14,7 @@ fn main() {
     tauri::async_runtime::spawn(server::run());
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, ipc::server_address::server])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
