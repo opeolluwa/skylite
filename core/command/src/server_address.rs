c082ec92cc338c7750c2f499fe742b6437bfc52a
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-11-10 17:49:24 +0100

    lint

--- core/command/src/server_address.rs
+++ core/command/src/server_address.rs
@@ -1,10 +1,13 @@
+use std::net::{IpAddr, Ipv4Addr};
+
 use local_ip_address::local_ip;
 use utils::pkg::CommandData;
 
 #[tauri::command]
-pub  fn server() -> Result<CommandData<String>, ()> {
-    let ip = local_ip().unwrap();
+pub fn server() -> CommandData<String> {
+    let default_ip_address = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+    let local_ip = local_ip().unwrap_or(default_ip_address);
     let port = 2105;
-    let server_address = format!("http://{}:{}", ip, port);
-    Ok(CommandData::ok("server address", server_address))
+    let server_address = format!("http://{}:{}", local_ip, port);
+    CommandData::ok("server address", server_address)
 }
--- core/command/src/wifi.rs
+++ core/command/src/wifi.rs
@@ -1,7 +1,13 @@
+use local_ip_address::local_ip;
 use utils::pkg::CommandData;
 
 #[tauri::command]
-pub fn is_connected_to_wifi() -> Result<CommandData<bool>, ()> {
-    // do business logic to see if device is connected to wifi
-    Ok(CommandData::ok("server address", true))
+pub fn is_connected_to_wifi() -> CommandData<bool> {
+    // the app would have a local ip address if it is connected to a network
+    // else it would crash, this is leveraged to check the network status
+    let has_ip_addr = local_ip().ok();
+    if has_ip_addr.is_none() {
+        return CommandData::ok("wifi status", false);
+    }
+    CommandData::ok("server address", true)
 }
